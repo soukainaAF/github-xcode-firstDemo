cb89dac9bf71dd85869f102fe4db757672861be3
ASKI Soukaina <S_aski@air-de-aski>
2017-06-18 18:12:09 +0200

    Initial Commit

---
+++ FirstDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstDemo
+//
+//  Created by ASKI Soukaina on 18/06/2017.
+//  Copyright © 2017 ASKI Soukaina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstDemo
+//
+//  Created by ASKI Soukaina on 18/06/2017.
+//  Copyright © 2017 ASKI Soukaina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
